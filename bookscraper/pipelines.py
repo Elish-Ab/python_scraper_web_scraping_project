5530aec24b14a2a1724045f8046bcac4d7e13d28
Elisha <elishabu28@gmail.com>
2024-03-06 13:52:40 +0300

    change in items--> category, product_typr, and price(euro to dollar)

--- bookscraper/pipelines.py
+++ bookscraper/pipelines.py
@@ -10,4 +10,31 @@ from itemadapter import ItemAdapter
 
 class BookscraperPipeline:
     def process_item(self, item, spider):
+        
+        adapter = ItemAdapter(item)
+
+        ## Strip all whitespaces from strings
+        field_names = adapter.field_names()
+        for field_name in field_names:
+            if field_name != "description":
+                value = adapter.get(field_name)
+                adapter[field_name] = value.strip()
+        
+        
+        ## Category & Product Type -----> switch to lowercase
+        lowercase_keys = ['category', 'product_type']
+        for lowercase_key in lowercase_keys:
+            value = adapter.get(lowercase_key)
+            adapter[lowercase_key] = value.lower()
+         
+         
+            
+            ## Price ----> convert to float
+        price_keys = ['price', 'price_excl_tax', 'price_incl_tax', 'tax']
+        for price_key in price_keys:
+            value = adapter.get(price_key)
+            value = value.replace('£', '')
+            adapter[price_key] = float(value)
+        
+        
         return item
